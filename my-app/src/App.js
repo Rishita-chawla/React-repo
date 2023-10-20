0b7006d9847a0529bd1d9f9001333a4b37a5ecd6
Rishita Chawla <rishitachawla1513@gmail.com>
2023-10-20 16:17:27 +0530

    Add initial files

---
+++ my-app/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Hello Everyone!</h2>
+        <h2>Day-1 task</h2>
+        <h1>MY FIRST REACT PROJECT</h1>
+        <h2>NAME:RISHITA CHAWLA</h2>
+        <h2>COURSE:BSc.(HONS)COMPUTER SCIENCE</h2>
+        <h2>COLLEGE:INDRAPRASTHA COLLEGE FOR WOMEN (DU)</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
